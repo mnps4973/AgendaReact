ea0be7a030d0b409341ec35148d700c8c6da92f4
Margarida <56021765+mnps4973@users.noreply.github.com>
2024-10-22 18:09:39 +0100

    calendar figured out

--- nextjs-dashboard/app/dashboard/agenda/mycalendar.tsx
+++ nextjs-dashboard/app/dashboard/agenda/mycalendar.tsx
@@ -1,12 +1,6 @@
-import {Calendar} from "@nextui-org/calendar";
-import {parseDate} from '@internationalized/date';
-import styles from '@/app/ui/dashboard/agenda/mycalendar.module.css';
-
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function MyCalendar() {
-    return (
-        <div className={styles.customCalendar} >
-            <Calendar calendarWidth="70vw" showMonthAndYearPickers />
-        </div>
-    );
+    return ( <Calendar />  );
 }
