5eac20a83c85749b283567624ab3883ec7455016
Margarida <56021765+mnps4973@users.noreply.github.com>
2024-10-31 16:31:03 +0000

    calendar's heights and widths done

--- nextjs-dashboard/app/dashboard/agenda/mycalendar.tsx
+++ nextjs-dashboard/app/dashboard/agenda/mycalendar.tsx
@@ -1,6 +1,15 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import styles from '@/app/ui/dashboard/agenda/calendarheader.module.css';
 
 export default function MyCalendar() {
-    return ( <Calendar />  );
+    return (
+        <>
+            <div className={styles.header}>
+                
+            </div>
+            <Calendar />
+        </>
+        
+    );
 }
